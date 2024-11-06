6ad3a090c0b60ec136fe520f3937dbaedc1de5ab
meow-20 <bansaripanchal2021@gmail.com>
2024-11-07 00:41:51 +0530

    routers added

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 const db = require('./config/mongoose-connection');
+const ownerRouter = require('./routes/ownerRouter');
+const productRouter = require('./routes/productRouter');
+const userRouter = require('./routes/userRouter');
 
 app.use(cookieParser());
 app.use(express.json());
@@ -11,9 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'ejs');
 
-
-app.get('/', function(req, res){
-    res.send('Hello World!');
-})
+app.use('/owner', ownerRouter);
+app.use('/products', productRouter);
+app.use('/user', userRouter);
 
 app.listen('3000');
