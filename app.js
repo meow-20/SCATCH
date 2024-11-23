7d8c903d012847bfee8e1031764a5b36b079fa25
BANSARI PANCHAL <88596007+meow-20@users.noreply.github.com>
2024-11-24 05:28:42 +0530

    app.js added

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6eb69d803e1ed41d70ed2deb0e0e2703e0784450fd508715541e42b662a56367
-size 1287
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const flash = require('connect-flash');
+// const config = require('config');
+
+const app = express();
+require('dotenv').config();
+
+const db = require('./config/mongoose-connection');
+const ownerRouter = require('./routes/ownerRouter');
+const productRouter = require('./routes/productRouter');
+const userRouter = require('./routes/userRouter');
+const indexRouter = require('./routes/index');
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+    expressSession({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.EXPRESS_SESSION_SECRET,
+    })
+);
+app.use(flash());
+app.set('view engine', 'ejs');
+
+// console.log(process.env.NODE_ENV);
+// console.log(process.env.JWT_KEY);
+
+app.get('/', indexRouter);
+app.get('/shop', indexRouter);
+app.get('/addtocart/:productid', indexRouter);
+app.get('/cart', indexRouter);
+
+app.use('/owner', ownerRouter);
+app.use('/products', productRouter);
+app.use('/users', userRouter);
+
+
+app.listen('3000');
